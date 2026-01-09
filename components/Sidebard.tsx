9d2583f53ab3f32a650042624225422897629697
rsmediazero <60850241+diena742@users.noreply.github.com>
2026-01-09 22:27:19 +0700

    [UPLOAD] via R2S

---
+++ components/Sidebard.tsx
@@ -0,0 +1,13 @@
+export function Sidebar() {
+  return (
+    <aside className="w-60 border-r p-4">
+      <ul className="space-y-2">
+        <li>Overview</li>
+        <li>Episodes</li>
+        <li>Series</li>
+        <li>Play</li>
+        <li>Sandbox</li>
+      </ul>
+    </aside>
+  );
+}
